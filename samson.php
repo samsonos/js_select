ac38058c65f0385f3d8b95bd0cdf5cc96898347a
Vitaly Iegorov <egorov@samsonos.com>
2014-08-22 15:09:51 +0300

    Updated module connector

--- samson.php
+++ samson.php
@@ -6,14 +6,8 @@ namespace samson\js\select;
  *
  * @package SamsonPHP
  * @author Vitaly Iegorov <vitalyiegorov@gmail.com>
- * @author Nikita Kotenko <nick.w2r@gmail.com>
  * @version 0.1
  */
 class Select extends \samson\core\CompressableExternalModule
-{	
-	/** Identifier */
-	protected $id = 'select';	
-	
-	/** Module dependencies */
-	protected $requirements = array('SamsonJS');
+{		
 }
